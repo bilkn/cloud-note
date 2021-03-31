8a1c4703221c8a938fec402b3f751c852b522340
bilkn <bilkan.konus@hotmail.com>
2021-03-31 20:49:38 +0300

    feat: add header container and home page

---
+++ src/containers/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from '../components';
+function HeaderContainer() {
+  return <Header>
+      <Header.Box>
+          <Header.SearchBox>
+              
+          </Header.SearchBox>
+      </Header.Box>
+  </Header>;
+}
+
+export default HeaderContainer;
