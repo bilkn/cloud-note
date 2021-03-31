0d596f895696a3f93206e5f9e8396f548aa01370
bilkn <bilkan.konus@hotmail.com>
2021-03-31 20:58:02 +0300

    feat: add global styles

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Header } from '../components';
 function HeaderContainer() {
-  return <Header>
-      <Header.Box>
-          <Header.SearchBox>
-              
-          </Header.SearchBox>
-      </Header.Box>
-  </Header>;
+  return (
+    <Header>
+      <Header.Wrapper>
+        <Header.Box>
+          <Header.SearchBox></Header.SearchBox>
+        </Header.Box>
+        <Header.Profile />
+      </Header.Wrapper>
+    </Header>
+  );
 }
 
 export default HeaderContainer;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'normalize.css';
+import { GlobalStyles } from './global-styles';
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
