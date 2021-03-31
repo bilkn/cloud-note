3a40b25dc1007b26a103c30da733db0af2c51535
bilkn <bilkan.konus@hotmail.com>
2021-03-31 20:29:49 +0300

    feat: add header

---
+++ src/components/header/styles/header.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.header``;
+export const Box = styled.div``;
+export const SearchBox = styled.input``;
+export const Profile= styled.div``;
+export const Icon = styled.div``;
+
+
