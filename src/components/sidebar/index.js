e5c228521a238b0e2edb02c6b804482a1b1e88f0
bilkn <bilkan.konus@hotmail.com>
2021-04-02 16:39:33 +0300

    feat: add aside

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 
-import { Container, Wrapper, Nav } from './styles/sidebar';
+import {
+  Container,
+  Wrapper,
+  Nav,
+  List,
+  Item,
+  Button,
+  ButtonLink,
+} from './styles/sidebar';
 
 function Sidebar({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
 
-Sidebar.Wrapper = function ({ children, ...restProps }) {
+Sidebar.Wrapper = function SidebarWrapper({ children, ...restProps }) {
   return <Wrapper {...restProps}>{children}</Wrapper>;
 };
 
-Sidebar.Nav = function ({ children, ...restProps }) {
+Sidebar.Nav = function SidebarNav({ children, ...restProps }) {
   return <Nav {...restProps}>{children}</Nav>;
+};
+
+Sidebar.List = function SidebarList({ children, ...restProps }) {
+  return <List {...restProps}>{children}</List>;
+};
+
+Sidebar.Item = function SidebarItem({ children, ...restProps }) {
+  return <Item {...restProps}>{children}</Item>;
+};
+
+Sidebar.Button = function SidebarButton({ children, ...restProps }) {
+  return (
+    <Item>
+      <Button {...restProps}>{children}</Button>
+    </Item>
+  );
+};
+
+Sidebar.ButtonLink = function SideBarButtonLink({ children, ...restProps }) {
+  return (
+    <Item>
+      <ButtonLink to="/" {...restProps}>
+        {children}
+      </ButtonLink>
+    </Item>
+  );
 };
 
 export default Sidebar;
--- src/components/sidebar/styles/sidebar.js
+++ src/components/sidebar/styles/sidebar.js
@@ -1,7 +1,36 @@
-import styled from "styled-components/macro"
+import styled from 'styled-components/macro';
+import { Link as ReactRouterLink } from 'react-router-dom';
 
-export const Container = styled.aside``
+export const Container = styled.aside`
+  background: #e9ecf5;
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  width: 100%;
+`;
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div``;
 
-export const Nav = styled.nav``
+export const Nav = styled.nav``;
+
+export const List = styled.ul`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 2em;
+`;
+
+export const Item = styled.li``;
+
+export const Button = styled.button`
+  background: none;
+  border: none;
+  bottom: 20px;
+  color: #515255;
+  position: relative;
+`;
+
+export const ButtonLink = styled(ReactRouterLink)`
+  color: #f1554c;
+  display: inline-block;
+`;
