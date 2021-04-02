71348f060999de902071c848735d600cf9c66aa0
bilkn <bilkan.konus@hotmail.com>
2021-04-02 19:28:06 +0300

    feat: add popover

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './header';
 export { default as Main } from './main';
 export { default as Sidebar } from './sidebar';
+export { default as Popover } from './popover';
---
+++ src/components/popover/styles/popover.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/macro';
+import { Link as ReactRouterLink } from 'react-router-dom';
+
+export const Container = styled.div``;
+
+export const List = styled.ul``;
+
+export const Item = styled.li``;
+
+export const ButtonLink = styled(ReactRouterLink)``;
+
+export const Line = styled.hr``
