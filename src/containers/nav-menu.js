1368bd584fbadcdb7df7df470190b3c10deabca8
bilkn <bilkan.konus@hotmail.com>
2021-04-16 15:18:56 +0300

    feat: add navigation-links fixture

--- src/containers/nav-menu.js
+++ src/containers/nav-menu.js
@@ -2,36 +2,26 @@ import React from 'react';
 import { useRouteMatch } from 'react-router';
 import { Navigation as Nav } from '../components';
 import { useMatchLastSubpath } from '../hooks';
+import NavLinks from '../fixtures/navigation-links.json';
 import * as Routes from '../constants/routes';
 
 export default function NavMenuContainer() {
   const { url } = useRouteMatch();
   const { matchSubpath } = useMatchLastSubpath();
 
-  // !!! May be refactored in the future.
+
   return (
     <Nav.Menu>
-      <Nav.MenuItem active={matchSubpath(Routes.PROFILE)}>
-        <Nav.MenuLink
-          to={`${url}${Routes.PROFILE}`}
-          active={matchSubpath(Routes.PROFILE)}
-        >
-          Edit Profile
-        </Nav.MenuLink>
-      </Nav.MenuItem>
-      <Nav.MenuItem active={matchSubpath(Routes.ACCOUNT)}>
-        <Nav.MenuLink to={Routes.ACCOUNT} active={matchSubpath(Routes.ACCOUNT)}>
-          Account Settings
-        </Nav.MenuLink>
-      </Nav.MenuItem>
-      <Nav.MenuItem active={matchSubpath(Routes.PASSWORD)}>
-        <Nav.MenuLink
-          to={`${url}${Routes.PASSWORD}`}
-          active={matchSubpath(Routes.PASSWORD)}
-        >
-          Password
-        </Nav.MenuLink>
-      </Nav.MenuItem>
+      {NavLinks.map(({ path, name }) => (
+        <Nav.MenuItem active={matchSubpath(path)}>
+          <Nav.MenuLink
+            to={`${url !== path ? url : ''}${path}`}
+            active={matchSubpath(path)}
+          >
+            {name}
+          </Nav.MenuLink>
+        </Nav.MenuItem>
+      ))}
     </Nav.Menu>
   );
 }
