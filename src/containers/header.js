21fc79d1f0dd743be0aeb5aeb5b02ce3ebd1adba
bilkn <bilkan.konus@hotmail.com>
2021-05-05 18:07:38 +0300

    feat: add conditional header buttons according to auth state

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Form, Header, Popover } from '../components';
+import { Form, Header, Popover, Avatar } from '../components';
+import { FirebaseAuthContext } from '../context';
 import { Search } from '@styled-icons/evil/Search';
 import { PopoverContainer } from './';
 import Picture from '../assets/man-1.png';
-import Avatar from '../components/avatar';
 import 'styled-components/macro';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
 import useQuery from '../hooks/useQuery';
-import { useWindowEvent, useWindowKey } from '../hooks';
+import { useWindowEvent, useWindowKey, useFirebaseAuth } from '../hooks';
 
 export default function HeaderContainer() {
   const [showPopover, setShowPopover] = useState(false);
+  const { currentUser } = useFirebaseAuth(FirebaseAuthContext);
   const { query, setQuery } = useQuery('search');
+
   useWindowEvent({
     events: [{ event: 'click' }],
     handlers: [() => setShowPopover(false)],
@@ -47,42 +49,49 @@ export default function HeaderContainer() {
             </Header.Icon>
           </Header.SearchBox>
         </Header.Box>
-        {/* <Header.Wrapper>
-          <Form.Button
-            to={ROUTES.SIGN_IN}
-            forwardedAs={ReactRouterLink}
-            css={`
-              background: none;
-              color: black;
-              margin: 0;
-              margin-right: 0.5em;
-              &:hover {
-                background: none;
-              }
-            `}
-          >
-            Sign In
-          </Form.Button>
-          <Form.Button
-          variant="red"
-            to={ROUTES.SIGN_UP}
-            forwardedAs={ReactRouterLink}
-            css={`
-              margin: 0;
-            `}
-          >
-            Sign Up
-          </Form.Button>
-        </Header.Wrapper> */}
-        <Avatar.Button
-          size="40"
-          onClick={handleAvatarClick}
-          data-testid="avatar"
-        >
-          <Avatar.Picture src={Picture} alt="Avatar" />{' '}
-          {/* !!! Add username to alt. */}
-          {showPopover && <PopoverContainer data-testid="popover" />}
-        </Avatar.Button>
+        <Header.Wrapper>
+          {currentUser ? (
+            <>
+              <Avatar.Button
+                size="40"
+                onClick={handleAvatarClick}
+                data-testid="avatar"
+              >
+                <Avatar.Picture src={Picture} alt="Avatar" />
+                {/* !!! Add username to alt. */}
+                {showPopover && <PopoverContainer data-testid="popover" />}
+              </Avatar.Button>
+            </>
+          ) : (
+            <>
+              <Form.Button
+                to={ROUTES.SIGN_IN}
+                forwardedAs={ReactRouterLink}
+                css={`
+                  background: none;
+                  color: black;
+                  margin: 0;
+                  margin-right: 0.5em;
+                  &:hover {
+                    background: none;
+                  }
+                `}
+              >
+                Sign In
+              </Form.Button>
+              <Form.Button
+                variant="red"
+                to={ROUTES.SIGN_UP}
+                forwardedAs={ReactRouterLink}
+                css={`
+                  margin: 0;
+                `}
+              >
+                Sign Up
+              </Form.Button>
+            </>
+          )}
+        </Header.Wrapper>
       </Header.Wrapper>
     </Header>
   );
--- src/hooks/useFirebase.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { FirebaseContext } from '../context';
-
-export default function useFirebase() {
-  return useContext(FirebaseContext);
-}
